39296f6c9d2122807f63f4d9693c6997b282fe24
Jay Bensal <jay.bensal@gmail.com>
2014-02-11 13:01:05 -0600

    add event listener in viz to start drawing graph

--- js/viz.js
+++ js/viz.js
@@ -1,19 +1,37 @@
 /*
  * Logic for D3 visualization of Enron Data Set
  * For CS 467, Spring 2014.
- * Authors: Jay Bensal, Raj Ramamurthy
+ * Authors: Mike Luo, Mike Styve, Jay Bensal, Fernando Araujo
  */
 
 /* Data storage */
 
 /* Load in the data */
 var loadData = function() {
+  /*
+  alert('hi');
+  console.log("inside load data")
+  // load json file
+
   
-  d3.json('', function () {
-    // callback function here
+  d3.json('test.json', function(json){
+      alert(hello + json);
+  }, function(error, rows){
+      if (error) {
+        console.log('Error loading player data: ', error);
+      }
     dataIsLoaded();
   });
-};
+  */
+
+  d3.json('/subset.json', function(json){
+      console.log("hi");
+  });
+
+dataIsLoaded();
+}; // end function
+
+
 
 /* The data is loaded at this point. */
 var dataIsLoaded = function() {
@@ -23,6 +41,10 @@ var dataIsLoaded = function() {
 
 /* Make a scatterplot graph for the life expectancies*/
 var makeGraph = function(){
-  
+  console.log("in make graph");
 
 }; // end makeGraph function
+
+window.addEventListener("DOMContentLoaded", function() {
+  loadData();
+}, false);
